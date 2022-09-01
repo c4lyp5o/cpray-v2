08f121e52d4709df68f5e7783f1fe8f8d63cd6a0
c4lyp5o <calypso.hellhammer@gmail.com>
2022-09-01 20:09:12 +0800

    vanilla for later editing

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      ≈<Component {...pageProps} />
+      <Component {...pageProps} />
     </Layout>
   );
 }
